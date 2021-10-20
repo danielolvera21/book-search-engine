426aca72cbc7cbb56e78b52bde7d67837c30cab1
Daniel O <danielolvera2188@gmail.com>
2021-10-20 17:37:35 -0600

    pushed to heroku

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Book Search Engine Starter Code
+# Book Search Engine
+
+## Description
+
+A Google Books API search engine refactored from a RESTful API to a GraphQL API built with the Apollo Server. Built using the MERN Stack.
+
+## Technologies Used
+
+- React
+- MongoDB
+- Express.js
+- Node.js
+- GraphQL
+- JSONWebToken
+
+## Errors
+
+Was not able to fully complete challenge due to time constraints. Will work to complete app and make it fully functional after challenge is submitted.
+
+Also had an issue getting the back end to start using npm start.
+
+## Screenshots
+
+## Links
+
+Github Repo: https://github.com/danielolvera21/book-search-engine
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -12,32 +12,9 @@ const SavedBooks = () => {
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || data?.userData || {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -64,7 +41,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (!userData) {
     return <h2>LOADING...</h2>;
   }
 
