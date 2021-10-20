ef51486821453a953be331ced9a9f9406af85fcf
Daniel O <danielolvera2188@gmail.com>
2021-10-20 15:59:21 -0600

    added queries and mutations to utils folder

--- server/server.js
+++ server/server.js
@@ -24,17 +24,13 @@ const startServer = async () => {
 
 startServer();
 
-app.use(express.urlencoded({ extended: false })); //starter code had this as 'true', I changed to 'false'
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
 
 
 db.once('open', () => {
